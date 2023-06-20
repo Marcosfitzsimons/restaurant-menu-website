909a51ee8ec5ba431736e1e3639e2c488dafa34e
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-20 10:38:14 -0300

    Add hero component

---
+++ components/hero.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+    <section className="relative grid w-full items-center pb-16 pt-6 after:absolute after:inset-0 after:z-10 after:bg-gradient-to-b after:from-transparent after:to-black/40">
+      <Image
+        src="/images/hero.jpg"
+        alt="animales"
+        fill
+        className="-z-10 object-cover"
+        sizes="(max-width: 768px) 100vw"
+      />
+      <div className="flex max-w-[980px] flex-col items-center gap-14">
+        <h1 className="z-20 rounded-full border border-[#e1e7ef] bg-accent px-2 py-1 text-[#e1e7ef]">
+          Confitería Golfo Nuevo
+        </h1>
+
+        <h2 className="z-20 text-2xl font-extrabold leading-tight tracking-tighter text-white md:text-4xl">
+          ¿Qué te gustaría ordenar?
+        </h2>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
