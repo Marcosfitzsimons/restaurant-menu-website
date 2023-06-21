fc5e89e7efb72ee7e0118fc83586fb33c0864af5
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-20 22:26:35 -0300

    Add main section

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import Hero from "@/components/hero"
+import Main from "@/components/main"
 
 export default function IndexPage() {
   return (
     <div>
       <Hero />
-      <main className="container"></main>
+      <Main />
     </div>
   )
 }
