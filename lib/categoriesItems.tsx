596e97d1cf442cdfc8b45476b3ec690a3b2eb9bb
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-20 22:25:03 -0300

    Add categories

---
+++ lib/categoriesItems.tsx
@@ -0,0 +1,35 @@
+import { CategoryItem } from "@/types/category"
+import { Icons } from "@/components/icons"
+
+export const categoriesItems: CategoryItem[] = [
+  {
+    id: 1,
+    icon: <Icons.layoutGrid />,
+    text: "Todas",
+  },
+  {
+    id: 2,
+    icon: <Icons.utensilsCrossed />,
+    text: "Comidas",
+  },
+  {
+    id: 3,
+    icon: <Icons.cupSoda />,
+    text: "Bebidas",
+  },
+  {
+    id: 4,
+    icon: <Icons.coffee />,
+    text: "Confitería",
+  },
+  {
+    id: 5,
+    icon: <Icons.candy />,
+    text: "Golosinas",
+  },
+  {
+    id: 6,
+    icon: <Icons.iceCream />,
+    text: "Postres",
+  },
+]
