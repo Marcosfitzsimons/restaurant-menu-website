672016eed20cc3c735304014b8665f04af5ce242
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-21 14:21:36 -0300

    Change card background color on dark mode

--- components/card.tsx
+++ components/card.tsx
@@ -6,16 +6,16 @@ import { Button } from "./ui/button"
 
 const Card = () => {
   return (
-    <article className="shrink-0 text-card w-60 relative flex flex-col justify-between gap-24 rounded-[0.5rem] p-3 after:absolute after:rounded-[0.5rem] after:inset-0 after:z-10 after:bg-gradient-to-b after:from-transparent after:to-black/70 dark:text-white">
+    <article className="shrink-0 text-card w-60 relative flex flex-col justify-between gap-24 rounded-[1.5rem] py-5 px-3 after:absolute after:rounded-[1.5rem] after:inset-0 after:z-10 after:bg-gradient-to-b after:from-transparent after:to-black/90 dark:text-white">
       <Image
         src="/images/burger.jpg"
         alt="animales"
         fill
-        className="-z-10 rounded-[0.5rem] object-cover object-center"
+        className="-z-10 rounded-[1.5rem] object-cover object-center"
         sizes="(max-width: 768px) 100vw"
       />
 
-      <h5 className="z-20 w-8/12">Hamburguesa con queso</h5>
+      <h5 className="z-20 w-8/12 font-bold">Hamburguesa con queso</h5>
       <div className="z-20 flex justify-between">
         <div className="flex flex-col">
           <p className="">$2000</p>
