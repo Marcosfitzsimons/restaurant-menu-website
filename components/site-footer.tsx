db7220240e88aaa75f9bb836fce7456dbbde6ed2
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-20 23:08:40 -0300

    Add footer component

---
+++ components/site-footer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SiteFooter = () => {
+  return (
+    <footer className="w-full rounded-xl p-4">
+      <nav className="rounded-xl bg-card p-4">asdsad</nav>
+    </footer>
+  )
+}
+
+export default SiteFooter
