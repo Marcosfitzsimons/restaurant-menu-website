7975036b47c6136af6ec1359e617ddb9d6abe2ed
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-24 17:37:05 -0300

    Add api endpoints

--- config/site.ts
+++ config/site.ts
@@ -12,5 +12,18 @@ export const siteConfig = {
   ],
   links: {
     facebook: "https://www.facebook.com/profile.php?id=100076494237211",
+    instagram: "https://www.instagram.com/golfo_nuevoterminal/",
   },
+  endPoints: {
+    "getAll": {
+      "url": "http://127.0.0.1:1337/api/products/?populate=*"
+    },
+    "getPopulars": {
+      "url": "http://127.0.0.1:1337/api/products?populate=*&filters[0][popular][$eq]=true"
+    },
+    "getCategory": {
+      "url": "http://127.0.0.1:1337/api/products?populate=*&filters[0][categoria][$eq]=:category"
+    }
+    // getSingle: http://127.0.0.1:1337/api/products/:id?populate=*
+  }
 }
