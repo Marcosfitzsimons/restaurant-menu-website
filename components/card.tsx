da546b090b07ec5c0c0fb7d805a0496c8bd6e51c
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-24 19:41:28 -0300

    Add dynamic data

--- components/card.tsx
+++ components/card.tsx
@@ -5,22 +5,25 @@ import { Plus } from "lucide-react"
 
 import { Button } from "./ui/button"
 
-const Card = () => {
+const Card = ({ product }: any) => {
+  const productData = product.attributes
+  const productImgUrl =
+    product.attributes.imagen.data.attributes.formats.medium.url
   return (
     <article className="shrink-0 text-card w-60 relative flex flex-col justify-between gap-24 rounded-[1.5rem] py-5 px-3 after:absolute after:rounded-[1.5rem] after:inset-0 after:z-10 after:bg-gradient-to-b after:from-transparent after:to-black/90 dark:text-white">
       <Image
-        src="/images/burger.webp"
+        src={productImgUrl}
         alt="animales"
         fill
         className="-z-10 rounded-[1.5rem] object-cover object-center"
         sizes="(max-width: 768px) 100vw"
       />
 
-      <h5 className="z-20 w-8/12 font-bold">Hamburguesa con queso</h5>
+      <h5 className="z-20 w-8/12 font-bold">{productData.titulo}</h5>
       <div className="z-20 flex justify-between">
         <div className="flex flex-col">
-          <p className="">$2000</p>
-          <Link href="/" className="text-sm text-white">
+          <p className="">${productData.precio}</p>
+          <Link href={`/${product.id}`} className="text-sm text-white">
             ver más
           </Link>
         </div>
