3f6c604ed72bfb3993eb43e8cceaf5c6336a77ed
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-25 12:31:28 -0300

    Update categories items source

--- lib/categoriesItems.tsx
+++ lib/categoriesItems.tsx
@@ -4,31 +4,26 @@ import { Icons } from "@/components/icons"
 export const categoriesItems: CategoryItem[] = [
   {
     id: 1,
-    icon: <Icons.layoutGrid />,
-    text: "Todas",
-  },
-  {
-    id: 2,
     icon: <Icons.wan />,
     text: "Promos",
   },
   {
-    id: 3,
+    id: 2,
     icon: <Icons.utensilsCrossed />,
     text: "Comidas",
   },
   {
-    id: 4,
+    id: 3,
     icon: <Icons.coffee />,
-    text: "Cafetería",
+    text: "Cafeteria",
   },
   {
-    id: 5,
+    id: 4,
     icon: <Icons.croissant />,
     text: "Dulces",
   },
   {
-    id: 6,
+    id: 5,
     icon: <Icons.cupSoda />,
     text: "Bebidas",
   },
