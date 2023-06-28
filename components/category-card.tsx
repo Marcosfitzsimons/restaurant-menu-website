f2a3e7429997d84fac80fe5fb31edc0574331fd2
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-28 19:26:26 -0300

    .

--- components/category-card.tsx
+++ components/category-card.tsx
@@ -14,7 +14,7 @@ const CategoryCard = ({ product }: CardProps) => {
   return (
     <article className="w-full text-card relative max-w-[355px] flex flex-col justify-between gap-[75px] rounded-[1.5rem] py-5 px-3 after:absolute after:rounded-[1.5rem] after:inset-0 after:z-10 after:bg-gradient-to-b after:from-transparent after:to-black/90 dark:text-white">
       <Image
-        src={productImgUrl}
+        src={productImgUrl ? productImgUrl : ""}
         alt="animales"
         fill
         className="-z-10 rounded-[1.5rem] object-cover object-center"
