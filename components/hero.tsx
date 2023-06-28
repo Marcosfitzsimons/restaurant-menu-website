1ace9e1fa1ec4c0a0cd6dde532c6d5ac9b9cc083
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-28 10:05:25 -0300

    Update hero design

--- components/hero.tsx
+++ components/hero.tsx
@@ -14,9 +14,13 @@ const Hero = () => {
         sizes="(max-width: 768px) 100vw"
       />
       <div className="flex max-w-[980px] flex-col items-center gap-14">
-        <h1 className="z-20 rounded-md select-none border border-[#e1e7ef]/30 bg-accent/10 px-3 py-1 font-serif text-white shadow-md backdrop-blur">
-          Golfo Nuevo Confitería
-        </h1>
+        <div className="relative z-20 after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] after:shadow-highlight after:shadow-white/30 dark:after:shadow-highlight dark:after:accent/20 after:transition focus-within:after:shadow-accent dark:focus-within:after:shadow-accent ">
+          <h1 className="relative flex select-none items-center rounded-lg border border-[#e1e7ef]/20 bg-black/5 px-3 py-1 shadow-black/10 shadow-input font-serif text-white shadow-md backdrop-blur-md dark:bg-accent/5">
+            Golfo Nuevo Confitería
+            <Separator className="absolute left-[187px] w-full bg-[#e1e7ef]/30 sm:hidden" />
+            <Separator className="absolute right-[187px] w-full bg-[#e1e7ef]/30 sm:hidden" />
+          </h1>
+        </div>
 
         <h2 className="z-20 text-2xl font-extrabold leading-tight tracking-tighter text-white md:text-4xl">
           ¿Qué te gustaría ordenar?
