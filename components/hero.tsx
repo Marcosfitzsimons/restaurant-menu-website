f4966e3140e2817465232c567c06f18425bb35b1
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-06-28 10:12:12 -0300

    Fix typing error

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
       />
       <div className="flex max-w-[980px] flex-col items-center gap-14">
         <div className="relative z-20 after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] after:shadow-highlight after:shadow-white/30 dark:after:shadow-highlight dark:after:accent/20 after:transition focus-within:after:shadow-accent dark:focus-within:after:shadow-accent ">
-          <h1 className="relative flex select-none items-center rounded-lg border border-[#e1e7ef]/20 bg-black/5 px-3 py-1 shadow-black/10 shadow-input font-serif text-white shadow-md backdrop-blur-md dark:bg-accent/5">
+          <h1 className="relative flex select-none items-center rounded-lg border border-[#e1e7ef]/20 bg-black/5 px-3 py-1 shadow-black/10 shadow-input font-serif text-white backdrop-blur-md dark:bg-accent/5">
             Golfo Nuevo Confitería
             <Separator className="absolute left-[187px] w-full bg-[#e1e7ef]/30 sm:hidden" />
             <Separator className="absolute right-[187px] w-full bg-[#e1e7ef]/30 sm:hidden" />
